bf5121eec6ff4fda9a5b028924d057ad5b43897e
Maulana Imam Malik <maulanaimammalik4@gmail.com>
2023-07-21 14:02:12 +0700

    add owner content and modify kasir index

--- api/proxy.js
+++ api/proxy.js
@@ -9,6 +9,8 @@ const apiProxy = createProxyMiddleware({
 });
 
 export default function handler(req, res) {
+  // Set the req.url to the correct endpoint URL
+  req.url = `/api/${req.url.split('/api/')[1]}`;
   // This will proxy any incoming request to the HTTP API
   apiProxy(req, res);
 }
