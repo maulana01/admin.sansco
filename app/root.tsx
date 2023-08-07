fd7efca26e91685649f502faa7d0b980292ec774
Maulana Imam Malik <maulanaimammalik4@gmail.com>
2023-08-07 11:17:52 +0700

    add styling on root

--- app/root.tsx
+++ app/root.tsx
@@ -11,12 +11,22 @@ export default function App() {
   return (
     <html lang='en'>
       <head>
+        <style>
+          {`body {
+            margin: 0;
+            padding: 0;
+          }
+
+          a {
+            text-decoration: none;
+          }`}
+        </style>
         <meta charSet='utf-8' />
         <meta name='viewport' content='width=device-width,initial-scale=1' />
         <Meta />
         <Links />
       </head>
-      <body style={{ margin:0 }}>
+      <body>
         <Outlet />
         <ScrollRestoration />
         <Scripts />
